9622d1be30232f2d521ffdb5a52b56cebb337700
Brian Gonzalez <brian.gonzalez@geni.com>
2012-10-11 14:41:08 -0700

    added request_helpers module to help scope css

---
+++ application/helpers/request_helpers.rb
@@ -0,0 +1,16 @@
+# ================
+# = Request Helpers =
+# ================
+module Sinatra
+  module RequestHelpers
+
+    def underscored_path
+      p = request.env['REQUEST_PATH']
+      p.slice!(0)
+      p.gsub '/', '_'
+    end
+
+  end
+
+  helpers RequestHelpers
+end
