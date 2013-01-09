fdf3f432f444627db753ff8061bd017c2a678db8
Brian Gonzalez <brian.gonzalez@geni.com>
2013-01-08 17:41:44 -0800

    remove cache

--- application/cache.rb
+++
@@ -1,4 +0,0 @@
-use Rack::Cache,
-  :metastore   => 'file:./public/cache/rack/meta',  # change these top be somewhere like /var/cache/rack/meta
-  :entitystore => 'file:./public/cache/rack/body',  # change these top be somewhere like /var/cache/rack/meta
-  :verbose     => true
