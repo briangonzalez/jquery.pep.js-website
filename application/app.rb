4e88b9f98e738c006270a7c7a7fa116b66125090
Brian Gonzalez <brian.gonzalez@geni.com>
2013-06-26 09:09:43 -0700

    added in file size

--- application/app.rb
+++ application/app.rb
@@ -5,6 +5,16 @@ end
 
 configure do
   set :demos, YAML.load_file( File.join( settings.views, 'demos.yml' )  )
+
+  set :pep_path, './public/javascripts/pep/dist/jquery.pep.min.js'
+  `tar -cvzf #{settings.pep_path}.gz #{settings.pep_path}`
+  set :pep_size, File.size("#{settings.pep_path}.gz")/1000
+end
+
+helpers do
+  def pep_size
+    settings.pep_size
+  end
 end
 
 before do
